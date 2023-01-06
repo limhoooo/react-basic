365fed94e606bed48a1071b227eca4dfccd27284
lim ho <dlagh123@gmail.com>
2023-01-06 13:55:57 +0900

    router 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Other from "./pages/Other";
 function App() {
-  return <div></div>;
+  const rand_0_9 = Math.floor(Math.random() * 10);
+  return (
+    <BrowserRouter>
+      <Link to="/">home</Link>
+      <div> </div>
+      <Link to={`/about/${rand_0_9}`}>about</Link>
+      <div> </div>
+      <Link to="/other">other</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/:id" element={<About />} />
+        <Route path="/other" element={<Other />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
