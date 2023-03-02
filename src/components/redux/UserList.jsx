8b1ba06064e437f86f9c30a656dbc3bf30898ef8
lim ho <dlagh123@gmail.com>
2023-03-02 13:59:51 +0900

    redux userlist 조건부 랜더링 수정

--- src/components/redux/UserList.jsx
+++ src/components/redux/UserList.jsx
@@ -4,12 +4,11 @@ const UserList = ({ users, getUsers }) => {
     useEffect(() => {
         getUsers()
     }, [getUsers])
-
-    if (users.length === 0) return <p>현재 유저정보 없음</p>
+    const userList = users.length === 0 ? <p>현재 유저정보 없음</p> : users.map(user => <li key={user.id}>{user.login}</li>)
     return (
         <>
             <h2>async Data + REDUX</h2>
-            <ul>{users.map(user => <li key={user.id}>{user.login}</li>)}</ul>
+            <ul>{userList}</ul>
         </>
     )
 };
